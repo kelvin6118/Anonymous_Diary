5a89a896ff80f37b1106440062a4bab15d37a099
Yan Wong <kelvinwong26546@gmail.com>
2022-09-22 18:13:18 +0800

    implemented create diary

--- src/util/fetchDiary.tsx
+++ src/util/fetchDiary.tsx
@@ -1,7 +1,29 @@
+import {Diary} from '../typing'
+
 export const fetchDiary = async () =>{
 
     const response = await fetch("https://kelvin-anonymous-diary.herokuapp.com/Diaries");
     const data = await response.json();
     return data;
     
+}
+
+export const createDiary = async (diary:Diary) => {
+
+    const headerDict = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Access-Control-Allow-Headers': 'Content-Type',
+      }
+
+
+    const response = await fetch('https://kelvin-anonymous-diary.herokuapp.com/Diaries', {
+        headers: new Headers(headerDict), 
+        method: 'POST', 
+        mode: 'cors',
+        body: JSON.stringify(diary)
+    });
+
+    const data = await response.json();
+    return data;
 }
