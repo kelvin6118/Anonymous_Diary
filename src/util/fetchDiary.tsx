bc8d44d773a3d0fe72c5771f88d0b58b53cd3698
Yan Wong <kelvinwong26546@gmail.com>
2022-09-22 18:41:02 +0800

    implemented fetch method to update diary

--- src/util/fetchDiary.tsx
+++ src/util/fetchDiary.tsx
@@ -9,17 +9,33 @@ export const fetchDiary = async () =>{
 }
 
 export const createDiary = async (diary:Diary) => {
-
     const headerDict = {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
         'Access-Control-Allow-Headers': 'Content-Type',
       }
 
-
     const response = await fetch('https://kelvin-anonymous-diary.herokuapp.com/Diaries', {
         headers: new Headers(headerDict), 
         method: 'POST', 
+        mode: 'cors',
+        body: JSON.stringify(diary)
+    });
+
+    const data = await response.json();
+    return data;
+}
+
+export const updateDiary = async (diary:Diary) => {
+    const headerDict = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Access-Control-Allow-Headers': 'Content-Type',
+      }
+
+    const response = await fetch('https://kelvin-anonymous-diary.herokuapp.com/Diaries', {
+        headers: new Headers(headerDict), 
+        method: 'PUT', 
         mode: 'cors',
         body: JSON.stringify(diary)
     });
