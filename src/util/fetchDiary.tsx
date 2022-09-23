658ca04f8f7c8488daa7056db2282ba5f05dfa35
Yan Wong <kelvinwong26546@gmail.com>
2022-09-24 02:51:58 +0800

    add type to data

--- src/util/fetchDiary.tsx
+++ src/util/fetchDiary.tsx
@@ -3,7 +3,7 @@ import {Diary} from '../typing'
 export const fetchDiary = async () =>{
 
     const response = await fetch("https://kelvin-anonymous-diary.herokuapp.com/Diaries");
-    const data = await response.json();
+    const data:Diary[] = await response.json();
     return data;
     
 }
